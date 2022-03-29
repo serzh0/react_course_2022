f46e9d71fed6b26b3fd4e1d037f5f4cb489f789a
Serzh <serzh152nn@gmail.com>
2022-03-29 16:05:46 +0300

    hw_less4_done

--- src/components/common/CardList/index.tsx
+++ src/components/common/CardList/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import style from './CardList.module.scss';
 
-// type CardListPropsType = {
-//   children: React.ReactNode;
-// };
-
 const CardList: React.FC = ({ children }) => <ul className={style.list}>{children}</ul>;
 
 export default CardList;
