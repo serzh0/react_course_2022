b9a7355aeb13fc7b8681b206a2c4275223ad9293
Serzh <serzh152nn@gmail.com>
2022-04-08 16:09:55 +0300

    lesson_6

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import PageWrapper from '../../common/PageWrapper';
+import { useLocation } from 'react-router-dom';
 import style from './MainPage.module.scss';
 import { data } from '../../../helpers';
 import UserList from './UserList';
@@ -16,16 +16,29 @@ import Button from '../../common/Button';
 
 const MainPage = () => {
   useEffect(() => console.log('MAIN_PAGE MOUNT'), []);
+
+  const { state } = useLocation() as { state: { isOpen: boolean } };
+
+  const location = useLocation();
+
+  // const mainState: { isOpen: boolean } = location.state as any;
+
   const [listVisible, setListVisible] = useState(false);
   const handler = () => {
     setListVisible((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (state?.isOpen) {
+      setListVisible(true);
+    }
+  }, [state?.isOpen]);
   return (
-    <PageWrapper>
+    <>
       <h1>User list:</h1>
       <Button title={listVisible ? 'Скрыть список' : 'Показать список'} onClick={handler} />
       {listVisible ? <UserList data={data} /> : <p>Список скрыт</p>}
-    </PageWrapper>
+    </>
   );
 };
 
